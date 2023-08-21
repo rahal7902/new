9f273b0db6d75121d59347d130f81b050d79e155
rahal7902 <rahalmohammed007@gmail.com>
2023-08-21 15:07:30 +0530

    Commit

---
+++ bin/final constant.dart
@@ -0,0 +1,19 @@
+void main(){
+  final String name="rahal";
+  const int age=65;
+  String collage="mtc";
+  print(name);
+  print(age);
+  add();
+  sub();
+}
+void add(){
+  int a=48,b=45;
+  int c=a+b;
+  print(c);
+}
+void sub(){
+  double x=65.2,y=23.9;
+  double z=x-y;
+  print(z);
+}
